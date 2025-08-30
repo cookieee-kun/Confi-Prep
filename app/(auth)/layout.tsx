7a5040d6b6cbc413cc3b54485dd180781a8bfe66
cookie-kun <piyushcalling09@gmail.com>
2025-08-30 18:05:34 +0530

    Implemented interview generation page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,6 @@ import { isAuthenticated } from '@/lib/actions/auth.action'
 import { redirect } from 'next/navigation'
 import React from 'react'
 import { ReactNode } from 'react'
-import { is } from 'zod/v4/locales'
 
 const AuthLayout = async ({ children } : {children: ReactNode}) => {
 	const issuserAuthenticated = await isAuthenticated();
